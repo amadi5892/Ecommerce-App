20cac7c14cd95b864ffab7303c5caac9a7e52858
amadi5892 <amadi5892@gmail.com>
2023-01-12 16:08:33 -0500

    created the navigation with react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 
+import Navigation from './routes/navigation/navigation.component';
+
 function App() {
   return (
     <Routes>
-      <Route>
-        
+      <Route path='/' element={<Navigation />}>
+
       </Route>
     </Routes>
   );
---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,35 @@
+import { Fragment } from "react";
+import { Outlet, Link } from "react-router-dom";
+
+const Navigation = () => {
+    return (
+        <Fragment>
+            <div className='navigation' >
+                <Link className='logo-container' to='/' >
+                    <span>Logo</span>
+                </Link>
+                <div className='nav-links-container' >
+                    <Link className='nav-link' to='/perfume'>
+                        Women's Perfume
+                    </Link>
+                    <Link className='nav-link' to='/cologne'>
+                        Men's Cologne
+                    </Link>
+                    <Link className='nav-link' to='/unisex'>
+                        Unisex Fragrance
+                    </Link>
+                    <Link className='nav-link' to='/shop'>
+                        Shop
+                    </Link>
+                    <Link className='nav-link' to='/auth'>
+                        Sign In
+                    </Link>
+
+                </div>
+            </div>
+            <Outlet />
+        </Fragment>
+    )
+}
+
+export default Navigation;

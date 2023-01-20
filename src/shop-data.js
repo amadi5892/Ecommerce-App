360c374b2ac61601b5087f9607eceeb55496a9bf
amadi5892 <amadi5892@gmail.com>
2023-01-20 06:12:21 -0500

    stored product objects and collections to firestore

---
+++ src/shop-data.js
@@ -0,0 +1,109 @@
+ const SHOP_DATA = [
+  {
+    title: 'mens',
+    items: [
+      {
+        "id": 1,
+        "name": "Victor Path",
+        "imageUrl": "victor_path",
+        "price": 112
+      },
+      {
+        "id": 2,
+        "name": "Black Currant",
+        "imageUrl": "black_currant",
+        "price": 89
+      },
+      {
+        "id": 3,
+        "name": "Alex Ware",
+        "imageUrl": "alex_ware",
+        "price": 85
+      },
+      {
+        "id": 4,
+        "name": "Thiha Thet Htoo",
+        "imageUrl": "thiha_thet_htoo",
+        "price": 120
+      },
+      {
+        "id": 5,
+        "name": "Virender Singh",
+        "imageUrl": "virender_singh",
+        "price": 78
+      },
+    ]
+  },
+  {
+    title: 'womens',
+    items: [
+      {
+        "id": 6,
+        "name": "Fulvio Ciccolo",
+        "imageUrl": "fulvio_ciccolo",
+        "price": 89
+      },
+      {
+        "id": 7,
+        "name": "Sara Saleh",
+        "imageUrl": "sara_saleh",
+        "price": 115
+      },
+      {
+        "id": 8,
+        "name": "Laura Chouette",
+        "imageUrl": "laura_chouette",
+        "price": 108
+      },
+      {
+        "id": 9,
+        "name": "Luwadlin Bosman",
+        "imageUrl": "luwadlin_bosman",
+        "price": 112
+      },
+      {
+        "id": 10,
+        "name": "Behnam Mohsenzadeh",
+        "imageUrl": "behnam_mohsenzadeh",
+        "price": 97
+      },
+    ]
+  },
+  {
+    title: 'unisex',
+    items: [
+      {
+        "id": 11,
+        "name": "Miska Sage",
+        "imageUrl": "miska_sage",
+        "price": 74
+      },
+      {
+        "id": 12,
+        "name": "Becky Sherburn",
+        "imageUrl": "becky_sherburn",
+        "price": 96
+      },
+      {
+        "id": 13,
+        "name": "Tron Le",
+        "imageUrl": "tron_le",
+        "price": 84
+      },
+      {
+        "id": 14,
+        "name": "Shahed Mufleh",
+        "imageUrl": "shahed_mufleh",
+        "price": 104
+      },
+      {
+        "id": 15,
+        "name": "Laura Jaeger",
+        "imageUrl": "laura_jaeger",
+        "price": 73
+      },
+    ]
+  }
+ ]
+ 
+ export default SHOP_DATA;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -14,7 +14,9 @@ import {
         getFirestore,
         doc,
         getDoc,
-        setDoc
+        setDoc,
+        collection,
+        writeBatch
     } from 'firebase/firestore'
 
 // firebase configuration
@@ -41,6 +43,19 @@ const firebaseConfig = {
   export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider);
 
   export const db = getFirestore();
+
+  export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = collection(db, collectionKey);
+    const batch = writeBatch(db);
+
+    objectsToAdd.forEach((object) => {
+      const docRef = doc(collectionRef, object.title.toLowerCase());
+      batch.set(docRef, object);
+    });
+
+    await batch.commit();
+    console.log('done');
+  };
 
   export const createUserDocumentFromAuth = async (userAuth, additionalInformation = {}) => {
     const userDocRef = doc(db, 'users', userAuth.uid);
